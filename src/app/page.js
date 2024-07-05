4e96d58a24eca225740896e43b58163ee704cd0a
hs.jung <jsusu7993@naturemobility.com>
2024-07-05 14:35:12 +0900

    Feat: 로그추가

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,11 @@ import { getMessaging, onMessage, getToken } from "firebase/messaging";
 const Index = () => {
   const [currentToken, setCurrentToken] = useState("");
 
-  const [log, setLog] = useState("");
+  const [logArr, setLogArr] = useState([]);
+
+  const log = (...args) => {
+    logArr.push(args.join(" "));
+  };
 
   const onMessageFCM = async () => {
     // 브라우저에 알림 권한을 요청합니다.
@@ -16,8 +20,8 @@ const Index = () => {
       "Notification" in window &&
       "PushManager" in window
     ) {
-      alert(2);
       const permission = await Notification.requestPermission();
+      log("permission:", permission);
       if (permission !== "granted") return;
 
       // 이곳에도 아까 위에서 앱 등록할때 받은 'firebaseConfig' 값을 넣어주세요.
@@ -31,6 +35,7 @@ const Index = () => {
       });
 
       const messaging = getMessaging(firebaseApp);
+      log("messaging:", JSON.stringify(messaging));
 
       // 이곳 vapidKey 값으로 아까 토큰에서 사용한다고 했던 인증서 키 값을 넣어주세요.
       getToken(messaging, {
@@ -38,24 +43,23 @@ const Index = () => {
           "BMu2AMDJl3W3lAPcXMNgK2QJi_eiB-UZhKV-IOWGYgNsViAheiITE00VomclG8zR-alVTSDY_ZnLk7CJPnzvWVw",
       })
         .then((currentToken) => {
-          console.log("currentToken", currentToken);
+          log("currentToken:", currentToken);
           if (currentToken) {
             // 정상적으로 토큰이 발급되면 콘솔에 출력합니다.
-            console.log(currentToken);
             setCurrentToken(currentToken);
           } else {
-            console.log(
+            log(
               "No registration token available. Request permission to generate one."
             );
           }
         })
         .catch((err) => {
-          console.log("An error occurred while retrieving token. ", err);
+          log("An error occurred while retrieving token. ", err);
         });
 
       // 메세지가 수신되면 역시 콘솔에 출력합니다.
       onMessage(messaging, (payload) => {
-        console.log("Message received. ", payload);
+        log("Message received. ", payload);
       });
     }
   };
@@ -67,13 +71,19 @@ const Index = () => {
   return (
     <div>
       <h1>hello world</h1>
-      <p
-        onClick={() => {
-          navigator.clipboard.writeText(currentToken);
-        }}
-      >
-        {currentToken}
-      </p>
+
+      <div className="p-2 border rounded brounded-md">
+        {logArr.map((log) => (
+          <p
+            className="p-1 border border-1"
+            onClick={() => {
+              navigator.clipboard.writeText(log);
+            }}
+          >
+            {log}
+          </p>
+        ))}
+      </div>
     </div>
   );
 };
